3232e94eb08ea4f7d22ee9b70696bd268d89f0e0
ffallah <farzad.fallah@gmail.com>
2024-07-31 16:44:23 +0200

    Update centos8_usf_repo.sh

--- scripts/centos8_usf_repo.sh
+++ scripts/centos8_usf_repo.sh
@@ -2,3 +2,4 @@
 
 sudo rm -f /etc/yum.repos.d/*
 sudo curl -o /etc/yum.repos.d/CentOS-Stream-USF.repo https://raw.githubusercontent.com/ffallah/linux/main/scripts/files/CentOS-Stream-USF.repo
+sudo dnf makecache --refresh
