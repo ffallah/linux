fe2dde41fb6e7d83edc5a779c88e2fa0011f39af
ffallah <farzad.fallah@gmail.com>
2024-09-05 20:35:33 +0200

    Update check_gpu.sh
    secured token

--- scripts/check_gpu.sh
+++ scripts/check_gpu.sh
@@ -2,13 +2,14 @@
 
 # Directory containing log files
 log_dir="/data/video/recordings/log"
+TOKEN = ""
 
 # Check if there are any .log files containing 'nvidia-smi'
 if grep -q "nvidia-smi" "$log_dir"/*.log; then
     echo "Found 'nvidia-smi' in one or more log files."
 
     sudo -u tno /home/tno/bin/boot_ssa.sh
-    /usr/bin/curl -s "https://api.telegram.org/bot308096639:AAESsT1NbR1mRJT3s7zjjYPw-Hndy7LrdHM/sendMessage?chat_id=177295964&text=$HOSTNAME" > /dev/null
+    /usr/bin/curl -s "https://api.telegram.org/bot308096639:$TOKEN/sendMessage?chat_id=177295964&text=$HOSTNAME" > /dev/null
 
 else
     echo "No logs contain 'nvidia-smi'."
