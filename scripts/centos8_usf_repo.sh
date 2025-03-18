5859d50b76851ea9e1405c86a7fedd16acb0f6e0
ffallah <farzad.fallah@gmail.com>
2025-03-18 17:55:18 +0100

    Delete scripts/centos8_usf_repo.sh

--- scripts/centos8_usf_repo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo rm -f /etc/yum.repos.d/*
-sudo curl -o /etc/yum.repos.d/CentOS-Stream-USF.repo https://raw.githubusercontent.com/ffallah/linux/main/scripts/files/CentOS-Stream-USF.repo
-sudo dnf makecache --refresh
